5faaa2b20657f1a4fbd29aa131750a51a0bc36eb
John J J <johnjjgnaaseelan@gmail.com>
2022-01-03 20:38:08 +0530

    added buttons and text entry

--- gui.py
+++ gui.py
@@ -1,6 +1,25 @@
 from tkinter import *
-window=Tk()
-btn=Button(window, text="This is Button widget", fg='blue')
-btn.place(x=80, y=100)
-window.title('Password Encryption')
+
+window = Tk()
+
+window.title("Password Encryption")
+
+def clicked():
+    pass
+
+lbl = Label(window, text="Enter a word for the password")
+lbl.grid(column=0, row=0)
+
+btn1 = Button(window, text="Enter", command=clicked)
+btn1.grid(column=1, row=0)
+
+txt = Entry(window,width=10)
+txt.grid(column=2,row=0)
+
+txt.focus()
+
+password_hint=txt.get()
+
+
+window.geometry('350x200')
 window.mainloop()
