3533bf4a72e6492052d22e09836ea45c0aa17d9a
John J J <johnjjgnaaseelan@gmail.com>
2022-01-03 20:14:03 +0530

    title

--- gui.py
+++ gui.py
@@ -2,6 +2,5 @@ from tkinter import *
 window=Tk()
 btn=Button(window, text="This is Button widget", fg='blue')
 btn.place(x=80, y=100)
-window.title('Hello Python')
-window.geometry("300x200+10+10")
+window.title('Password Encryption')
 window.mainloop()
