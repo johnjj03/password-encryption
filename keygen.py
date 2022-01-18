c4e09bc15a3d9a4f9362b571b6934c93d5867fdd
John J J <johnjjgnanaseelan@gmail.com>
2022-01-18 17:45:11 +0530

    added number database creation

--- keygen.py
+++ keygen.py
@@ -1,6 +1,9 @@
+#run this program only once
 from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
 fernet = Fernet(key)
 with open('filekey.key', 'wb') as filekey:
     filekey.write(key)
+with open('database.csv','w') as db:
+    pass
